16cac68d9ae50ea084a83a0dc564d803825b0fd6
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-15 10:27:28 +0200

    created new Component

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Title from './components/Title.vue';
+export default{
+  name:'app',
+  components: {
+    HelloWorld,
+    Title},
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+  <Title></Title>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>La mia prima app con Vite</h1>
+</template>
+
+<script>
+  export default{
+    name: 'title',
+    data(){
+        return{
+            
+        }
+    }
+  }
+</script>
+
+<style scoped>
+  h1{
+    text-align: center;
+  }
+</style>
